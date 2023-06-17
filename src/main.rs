6e0c6e2909977e2cf9f57cd8350722cc62ae7077
Mohammed Sazid Al Rashid <sazidozon@gmail.com>
2023-06-17 20:10:05 +0600

    add command line args for reading config

--- src/main.rs
+++ src/main.rs
@@ -2,57 +2,44 @@ mod flow;
 mod functions;
 mod kv_store;
 
-use kv_store::store::new as kv_store_new;
+use std::path::PathBuf;
+
+use clap::Parser;
 
 use crate::flow::Flow;
 use crate::functions::run;
+use kv_store::store::new as kv_store_new;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
+/// A load generator written in Rust
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Flow config in json
+    #[arg(long)]
+    flow: Option<String>,
+
+    /// Flow config file path
+    #[arg(long)]
+    flow_path: Option<PathBuf>,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let param_str = r#"
-    {
-        "functions": [
-            {
-                "LoadGen": {
-                    "spawn_rate": "1",
-                    "timeout": 1,
-                    "functions_to_execute": [
-                        {
-                            "HttpRequest": {
-                                "url": "https://reqres.in/api/users?page=1",
-                                "timeout": 300
-                            }
-                        },
-                        {
-                            "RunRhaiCode": {
-                                "code": "let user_id = http_response[\"data\"].sample().id;"
-                            }
-                        },
-                        {
-                            "RunRhaiCode": {
-                                "code": "print(\"Picked user_id: \" + user_id);"
-                            }
-                        },
-                        {
-                            "HttpRequest": {
-                                "url": "https://reqres.in/api/users/%|user_id|%",
-                                "timeout": 300
-                            }
-                        },
-                        {
-                            "RunRhaiCode": {
-                                "code": "let data = http_response.data; print(data.first_name + \" \" + data.last_name);"
-                            }
-                        }
-                    ]
-                }
-            }
-        ]
+    let args = Args::parse();
+
+    if args.flow.is_none() && args.flow_path.is_none() {
+        println!("ERROR: one of 'flow' or 'flow_path' must be provided.");
+        return Ok(());
     }
-    "#;
-    let flow: Flow = serde_json::from_str(param_str)?;
+
+    let mut flow = args.flow.unwrap_or("".into());
+    if let Some(path) = args.flow_path {
+        flow = std::fs::read_to_string(path)?;
+    }
+
+    let flow: Flow = serde_json::from_str(&flow)?;
     let (kv_handle, kv_tx) = kv_store_new().await;
 
     run::run_flow(flow, kv_tx).await?;
